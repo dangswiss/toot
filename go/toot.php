67f30799369ecb1d2c104f84acd9cbc3283b9a6c
Daniel Gurtner <dangswiss@gmail.com>
2016-11-15 00:34:53 -0800

    remove extra space after trump

--- go/toot.php
+++ go/toot.php
@@ -1,5 +1,5 @@
   <div class="well">
     <p><?php print $toot; ?></p>
   </div>
-  <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="trump<?php print $toot; ?>" data-url="." data-hashtags="NotMyPresident <?php print $locale; ?>" data-show-count="false">TooT</a>
+  <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="trump <?php print $toot; ?>" data-url="." data-hashtags="NotMyPresident <?php print $locale; ?>" data-show-count="false">TooT</a>
   <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
